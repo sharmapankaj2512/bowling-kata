40beaf3998c334ad5be99e28b061a4c714e0c92d
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-09-24 16:55:35 +0400

    all ones

--- Score.Tests/ScoreTest.cs
+++ Score.Tests/ScoreTest.cs
@@ -12,18 +12,32 @@ public class GameTest
         {
             game.Roll(0);
         }
-        Assert.AreEqual(game.Score(), 0);
+        Assert.AreEqual(0, game.Score());
+    }
+    
+    [Test]
+    public void AllOnes()
+    {
+        var game = new Game();
+        for (var i = 0; i < 20; i++)
+        {
+            game.Roll(1);
+        }
+        Assert.AreEqual(20, game.Score());
     }
 }
 
 public class Game
 {
+    private int _score;
+    
     public void Roll(int pins)
     {
+        _score += pins;
     }
 
     public int Score()
     {
-        return 0;
+        return _score;
     }
 }
